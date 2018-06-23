2b662ca43e453628e7f2d5c3e75420a10a4b3421
dmitrii <dmitry-orlov@hotmail.com>
2018-06-23 19:52:58 +0200

    added tested user service for publishing newly created users

--- src/controllers/auth/register/controller.js
+++ src/controllers/auth/register/controller.js
@@ -1,30 +1,34 @@
 const Account = require('../../../models/account');
 const bcrypt = require('bcrypt');
 const ErrorResponse = require('../../../responses/error-response');
+const UserService = require('../../../utils/user-serice');
 
 module.exports.register = async (req, res) => {
-	let { password, username } = req.body;
-	username = req.sanitize(username);
-	// Check if there is such a user
-	const user = await Account.findOne({ username });
-	if (user !== null) {
-		// User already exsists
-		const errorCode = 'UserExists';
-		req.log.info({ errorCode, username });
-		throw new ErrorResponse(409, errorCode, 'Username exists');
-	}
+  let { password, username } = req.body;
+  username = req.sanitize(username);
+  // Check if there is such a user
+  const user = await Account.findOne({ username });
+  if (user !== null) {
+    // User already exsists
+    const errorCode = 'UserExists';
+    req.log.info({ errorCode, username });
+    throw new ErrorResponse(409, errorCode, 'Username exists');
+  }
 
-	// Create new user
-	let newUser = new Account({
-		password: await bcrypt.hash(password, 10),
-		username,
-	});
+  // Create new user
+  let newUser = new Account({
+    password: await bcrypt.hash(password, 10),
+    username,
+  });
 
-	// Save in DB
-	const result = await newUser.save();
+  // Save in DB
+  const result = await newUser.save();
 
-	// Send the response
-	res.status(201).json({
-		success: true,
-	});
+  // Tell the user service to create a new user entry
+  const serviceJob = (new UserService()).createUserDocument(username);
+
+  // Send the response
+  res.status(201).json({
+    success: true,
+  });
 };
